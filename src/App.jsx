746600443d5f549d1046834a3df646331aa78c70
gauravgangwarweb <gauravgangwar501@gmail.com>
2024-06-22 15:04:24 +0530

    chore: Add Features component to App.jsx and update its layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Features from "./components/Features"
 import Hero from "./components/Hero"
 
 function App() {
   return (
     <div className="px-8">
       <Hero />
+      <Features />
     </div>
   )
 }
--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,11 +1,46 @@
 const Features = () => {
-    return (
-        <div className="grid grid-cols-3 gap-4 px-16">
-            <div className="flex flex-col items-center justify-center">
-                
-            </div>
-        </div>
-    );
+  return (
+    <div className="grid grid-cols-3 gap-4 px-16 mt-32">
+      <div className="flex flex-col items-center justify-center">
+        <img loading="lazy" src="invest.png" alt="invest" />
+        <h4 className="text-2xl font-bold text-[#20202c] mt-4">
+          Invest with Confidence
+        </h4>
+        <h6 className="text-lg font-bold text-[#6943e0] mt-2">
+          You Benefit from Pump and Dump
+        </h6>
+        <p className="text-lg text-[#000154] mt-2 text-center px-4">
+          Price increases with every trade, ensuring all traders benefit even
+          during Pump and Dump scenarios
+        </p>
+      </div>
+      <div className="flex flex-col items-center justify-center">
+        <img loading="lazy" src="referral.png" alt="referral" />
+        <h4 className="text-2xl font-bold text-[#20202c] mt-4">
+          Referral Growth
+        </h4>
+        <h6 className="text-lg font-bold text-[#6943e0] mt-2">
+          Build wealth through referrals
+        </h6>
+        <p className="text-lg text-[#000154] mt-2 text-center px-4">
+          Earn recurring 5% referral income for every trade made by your r
+          eferrals, FOREVER
+        </p>
+      </div>
+      <div className="flex flex-col items-center justify-center">
+        <img loading="lazy" src="guaranteed.png" alt="guaranteed" />
+        <h4 className="text-2xl font-bold text-[#20202c] mt-4">
+          Guaranteed Liquidity
+        </h4>
+        <h6 className="text-lg font-bold text-[#6943e0] mt-2">
+          Get Paid at Every Sale Without Worry
+        </h6>
+        <p className="text-lg text-[#000154] mt-2 text-center px-4">
+          Our patented 1DEX ensures liquidity under all conditions
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Features;
